945d76ea12ccf0354fe5b1f549420b3f03662df4
dolar <dolarsingh11@gmail.com>
2025-12-07 03:01:55 +0530

    add logo svg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
     title: "appxlab.space - Lightning Fast App Development",
     description: "We design lightning fast apps for the world",
+    icons: {
+        icon: "/favicon.svg",
+    },
 };
 
 export default function RootLayout({
